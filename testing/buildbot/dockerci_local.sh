b7bb190369ba86f3cbfd8937a0c335203b0b9263
Daniel Mizyrycki <daniel@dotcloud.com>
2013-09-21 20:15:44 -0700

    testing infrastructure, issue #1800: Base docker PRs and commit testing on /Dockerfile

--- testing/buildbot/dockerci_local.sh
+++ testing/buildbot/dockerci_local.sh
@@ -1,8 +1,14 @@
 #!/bin/sh -e
 # This is a one time script to prepare docker-ci
 
+# Build docker container
+cd /go/src/github.com/dotcloud/docker; docker build -t docker .
+
+# Build docker-test container
+cd testing/docker-test; docker build -t test_docker .
+
 # Build docker nightly release container
-cd /go/src/github.com/dotcloud/docker/testing/nightlyrelease; docker build -t dockerbuilder .
+cd ../nightlyrelease; docker build -t dockerbuilder .
 
 # Self removing
 echo -e '#!/bin/sh -e\nexit 0\n' > /etc/rc.local
